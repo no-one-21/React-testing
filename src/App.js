6d925cbe0b374f52e50865508581a14723429113
no-one-21 <rohitjadaun3@gmail.com>
2019-02-20 19:44:02 +0530

    testing counter display app

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter : 0
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div data-test="component-app"> 
+         <h1 data-test="counter-display"> current value of counter is {this.state.counter}</h1>
+         <button data-test="increment-button"
+          onClick={()=>this.setState({counter:this.state.counter + 1})}> 
+         Click me , to increase the value of counter</button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,52 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme , {shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+Enzyme.configure({adapter:new EnzymeAdapter()});
+
+const setup = (props={} , state=null) =>{
+  const wrapper = shallow(<App {...props}/>)
+  if(state)  wrapper.setState(state);
+  return wrapper;
+}
+
+const findByTestAttr = (wrapper,val) =>{
+  return wrapper.find(`[data-test="${val}"]`);
+}
+
+//shallow function takes jsx , and is is helpful in testing one level deep componet only!!
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper,'component-app');
+  expect(appComponent.length).toBe(1);
+});
+
+test('render increment button',()=>{
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper,'increment-button')
+  expect(button.length).toBe(1);
+});
+
+test('render counter display',()=>{
+  const wrapper = setup();
+  const counterdisplay = findByTestAttr(wrapper,'counter-display')
+  expect(counterdisplay.length).toBe(1);
+});
+
+test('counter starts at 0',()=>{
+  const wrapper=setup();
+   const initialCounterState = wrapper.state('counter');
+  expect(initialCounterState).toBe(0);
+});
+
+// enzyme simulate can be used to simulate the button click
+test('when button clicke counter incremented in display',()=>{
+  const counter = 7;
+  const wrapper = setup(null, {counter});
+  const button = findByTestAttr(wrapper,'increment-button')
+  button.simulate('click');
+  wrapper.update();
+  const counterdisplay = findByTestAttr(wrapper,'counter-display')
+  expect(counterdisplay.text()).toContain(counter +1 )
 });
